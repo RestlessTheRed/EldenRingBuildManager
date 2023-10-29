ccb0f7d8ca572a62beda4460c19f6fd8ff58c50d
Restless <None>
2023-10-30 00:34:34 +0300

    Can read variables from env too

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import json
+import os
 
 ENV_FILENAME = 'config.json'
+ENV_VARIABLES = ['ACCESS_TOKEN', 'CLIENT_ID', 'BOT_NICK']
 
 
 def get_config():
     with open(ENV_FILENAME) as f:
         config = json.load(f)
+    for variable in ENV_VARIABLES:
+        if not config.get(variable, ''):
+            config[variable] = os.environ.get(variable)
     return config
